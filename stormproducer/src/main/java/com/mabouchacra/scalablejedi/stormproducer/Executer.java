954c10817ec64cd59988ff9eb512d3f59f58e52a
greg <greg@localhost.localdomain>
2017-06-20 14:54:03 +0200

    Producer working

--- stormproducer/src/main/java/com/mabouchacra/scalablejedi/stormproducer/Executer.java
+++ stormproducer/src/main/java/com/mabouchacra/scalablejedi/stormproducer/Executer.java
@@ -1,13 +1,10 @@
 package com.mabouchacra.scalablejedi.stormproducer;
 
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by marc on 19/04/17.
@@ -15,13 +12,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Component
 public class Executer {
 
-    public static AtomicInteger count = new AtomicInteger(1);
+	public static AtomicInteger count = new AtomicInteger(1);
 
-    @Scheduled(fixedRate = 2000)
-    public void attack(){
-        ConnectionFactory connectionFactory = new CachingConnectionFactory();
-        AmqpTemplate template = new RabbitTemplate(connectionFactory);
-        System.out.println("Sending...");
-        template.convertAndSend("jedi-queue", "Attack!!! Stormtrooper!! N°:"+ count.getAndIncrement());
-    }
+	private RabbitTemplate template;
+
+ 	public Executer(RabbitTemplate template) {
+		this.template = template;
+	}
+
+	@Scheduled(fixedRate = 2000)
+	public void attack() {		 
+		template.convertAndSend("jedi-queue", "Attack!!! Stormtrooper!! N°:" + count.getAndIncrement());
+	}
 }
