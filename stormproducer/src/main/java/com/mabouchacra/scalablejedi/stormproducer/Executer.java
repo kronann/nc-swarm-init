7d631f985f9bbc59da2ae47c79bf6f688e35ac71
Marc ABOU CHACRA <marc.abouchacra@gmail.com>
2017-07-06 18:17:50 +0200

    Config spring-boot pour créer la queue rabbit autamtiquement si elle n'existe pas

--- stormproducer/src/main/java/com/mabouchacra/scalablejedi/stormproducer/Executer.java
+++ stormproducer/src/main/java/com/mabouchacra/scalablejedi/stormproducer/Executer.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import static com.mabouchacra.scalablejedi.stormproducer.StormproducerApplication.JEDI_QUEUE;
+
 /**
  * Created by marc on 19/04/17.
  */
@@ -16,8 +18,6 @@ import org.springframework.stereotype.Component;
 public class Executer {
 
 	public static AtomicInteger count = new AtomicInteger(1);
-
-	private static final String JEDI_QUEUE = "jedi-queue";
 
 	private RabbitAdmin admin;
 
--- stormproducer/src/main/java/com/mabouchacra/scalablejedi/stormproducer/StormproducerApplication.java
+++ stormproducer/src/main/java/com/mabouchacra/scalablejedi/stormproducer/StormproducerApplication.java
@@ -1,13 +1,35 @@
 package com.mabouchacra.scalablejedi.stormproducer;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
 public class StormproducerApplication {
+
+	public static final String JEDI_QUEUE = "jedi-queue";
+
+	@Bean
+	Queue queue() {
+		return new Queue(JEDI_QUEUE, true);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange("spring-boot-exchange");
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with(JEDI_QUEUE);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StormproducerApplication.class, args);
