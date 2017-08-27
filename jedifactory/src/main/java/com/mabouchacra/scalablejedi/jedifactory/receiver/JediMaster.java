a9094c640431b2f7a8f7ddd00aeb78f828111451
greg <greg@localhost.localdomain>
2017-08-27 17:26:07 +0200

    ajout d'un début de conf pour stack deploy (à compléter) ainsi que les étapes à jouer - import inutile dans le code

--- jedifactory/src/main/java/com/mabouchacra/scalablejedi/jedifactory/receiver/JediMaster.java
+++ jedifactory/src/main/java/com/mabouchacra/scalablejedi/jedifactory/receiver/JediMaster.java
@@ -3,15 +3,11 @@ package com.mabouchacra.scalablejedi.jedifactory.receiver;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.CountDownLatch;
-
 /**
  * Created by marc on 19/04/17.
  */
 @Component
 public class JediMaster {
-
-    private static CountDownLatch latch = new CountDownLatch(1);
 
     @RabbitListener(queues = {"jedi-queue"}, containerFactory = "myRabbitListenerContainerFactory")
     public void defend(String message) throws InterruptedException {
--- stormproducer/src/test/java/com/mabouchacra/scalablejedi/stormproducer/StormproducerApplicationTests.java
+++ stormproducer/src/test/java/com/mabouchacra/scalablejedi/stormproducer/StormproducerApplicationTests.java
@@ -1,9 +1,7 @@
 package com.mabouchacra.scalablejedi.stormproducer;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 //@RunWith(SpringRunner.class)
 @SpringBootTest
